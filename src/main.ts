7d93111fe9bbb90cf78ea65b6b9f79b1810c7553
Anyleve <anyleve123@gmail.com>
2026-03-16 12:14:04 +0300

    lab1

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { join } from 'path';
+import * as express from 'express';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(express.static(join(__dirname, '..', 'public')));
+
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
+}
+bootstrap();
